7b7de449ccb0291e18a11d6f76a1269ea56f6b75
Anand Graves <anand@voorhoede.nl>
2018-12-03 22:22:09 +0100

    refactor(get-ingredients): error handling

--- get-ingredients.js
+++ get-ingredients.js
@@ -14,13 +14,12 @@ function hasText(arr) {
 module.exports = function (req, res) {
 	const imageUrl = req.body.imageUrl
 	console.log('imageUrl', imageUrl)
-	/*
+	
 	if (!imageUrl) {
 		const message = 'imageUrl is missing'
-		res.send(message)
+		res.json({ error: message })
 		throw new Error(message)
 	}
-	*/
 
 	const client = new vision.ImageAnnotatorClient()
 
@@ -30,7 +29,7 @@ module.exports = function (req, res) {
 		.then(results => {
 			if (!results.length) {
 				const message = 'Could not detect text in the uploaded image. Please try another image.'
-				res.send(message)
+				res.json({ error: message })
 				throw new Error(message)
 			}
 
@@ -38,7 +37,7 @@ module.exports = function (req, res) {
 			
 			if (!hasText(detections)) {
 				const message = 'It seems the uploaded photo doesn\'t contain ingredients. Please upload another photo.'
-				res.send(message)
+				res.json({ error: message })
 				throw new Error(message)
 			}
 
@@ -52,7 +51,7 @@ module.exports = function (req, res) {
 
 			if (ingredientsIndex === -1) {
 				const message = 'Could not find the text "ingredients". Please upload another photo.'
-				res.send(message)
+				res.json({ error: message })
 				throw new Error(message)
 			}
 			// Remove all text before the lowercased text 'ingrediënten'
@@ -69,7 +68,8 @@ module.exports = function (req, res) {
 			})
 			const notesIndex = afterIngredients.findIndex((element, index) => {
 				if ((element === 'kan' && afterIngredients[index + 1] === 'sporen') ||
-				((element === 'koel' || element === 'donker') && afterIngredients[index + 1] === 'bewaren')) {
+				((element === 'koel' || element === 'donker') && afterIngredients[index + 1] === 'bewaren') ||
+				(element === 'voedingswaarde' && afterIngredients[index + 1] === 'per')) {
 					return true
 				}
 				return false
@@ -79,6 +79,7 @@ module.exports = function (req, res) {
 			let dotAfterIngredients
 			if (dotAfterIngredientsIndex !== -1) {
 				dotAfterIngredients = afterIngredients.slice(0, dotAfterIngredientsIndex + 1)
+				console.log('dotAfterIngredients EXISTS!')
 			} 
 			else {
 				if (notesIndex !== -1) {
@@ -87,16 +88,9 @@ module.exports = function (req, res) {
 			}
 
 			const fullText = results[0] // only for testing
-			const output = `
-				<p>after ingredients: ${afterIngredients}</p>
-				<p>${results[0].textAnnotations.description}</p>
-				<p>Full text annotation: ${results[0].fullTextAnnotation.text}</p>
-				<p>Dot after ingredients: ${dotAfterIngredients}</p>
-				<p>Notes ingredients: ${notesIngredients}</p>
-			`
-			
-			//res.send(output)
-			console.log('dotAfterIngredients', dotAfterIngredients)
+			// console.log('fullText', fullText)
+			// console.log('notesIngredients', notesIngredients)
+			// console.log('dotAfterIngredients', dotAfterIngredients)
 			res.json(dotAfterIngredients)
 		})
 		.catch(err => {
